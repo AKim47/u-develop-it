7a3b8baafcbc3796886b5df4818d67d2fffc4391
AKim47 <abel.kim.22@gmail.com>
2021-12-30 14:26:23 -0600

    added SQL to server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Ka1202!@$%',
+        database: 'election'
+    },
+    console.log('Connected to the election dataase.')
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
@@ -14,6 +26,10 @@ app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
